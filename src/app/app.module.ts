cc55107668624d03f526820b747d03e8c713e316
anaval-dev <anaval.dev@gmail.com>
2022-02-21 15:03:45 -0700

    hey component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeyComponent } from './exercise1-your-first-component/hey/hey.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeyComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/exercise1-your-first-component/hey/hey.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hey',
+  templateUrl: './hey.component.html',
+  styleUrls: ['./hey.component.scss']
+})
+export class HeyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
